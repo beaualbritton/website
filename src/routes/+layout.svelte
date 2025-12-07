86054ab3cb72649b084ead09094046165ac03627
beau albritton <albrittoncb@gmail.com>
2025-12-06 23:20:12 -0500

    fix: styling and project route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/navbar.svelte';
 	
 	let { children } = $props();
 </script>
@@ -9,19 +10,19 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-
-<div class="min-h-screen bg-[#2b3339] overflow-hidden">
-	<main class="max-w-2xl mx-auto py-16 px-6">
-		<div class="prose prose-invert max-w-none
-			prose-headings:text-[#d3c6aa] prose-headings:font-normal
-			prose-p:text-[#d3c6aa] prose-p:leading-relaxed
-			prose-a:text-[#a7c080] prose-a:no-underline hover:prose-a:text-[#83c092]
-			prose-strong:text-[#d3c6aa]
+<div class="min-h-screen bg-main overflow-hidden flex flex-row">
+	<Navbar/>
+	<main class="flex justify-start items-center w-full max-w-full">
+		<div class="text-main prose prose-invert max-w-full
+      prose-headings:text-[#d3c6aa]
+			prose-p:text-main prose-p:leading-relaxed
+			prose-a:text-green prose-a:no-underline hover:prose-a:text-aqua
+			prose-strong:text-main
 			prose-code:rounded prose-code:font-normal
-			prose-pre:bg-[#374247] prose-pre:border-0
-			prose-ul:text-[#d3c6aa] prose-ol:text-[#d3c6aa]
-			prose-li:marker:text-[#a7c080]
-			prose-blockquote:border-l-[#a7c080] prose-blockquote:text-[#d3c6aa]">
+			prose-pre:bg-surface prose-pre:border-0
+			prose-ul:text-main prose-ol:text-main
+			prose-li:marker:text-green
+			prose-blockquote:border-l-green prose-blockquote:text-main">
 			{@render children()}
 		</div>
 	</main>
