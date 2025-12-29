61dbd68d586e8529904d08a7ba2fa4a4c1586d9b
beau albritton <albrittoncb@gmail.com>
2025-12-28 20:57:16 -0500

    new pages + updating homepage

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,23 @@
+<main class ="w-3xl">
+  <h1>about</h1>
+
+  <div class = "flex flex-row w-full">
+    <div class = "flex flex-col w-1/2">
+      i enjoy:
+      <ul class="mt-2">
+        <li>full-stack development</li>
+        <li>distributed systems</li>
+        <li>thinking about thinking</li>
+      </ul>
+    </div>
+    <div class = "flex flex-col w-1/2">
+       on thin ice:
+        <ul class="mt-2">
+          <li>new jersey (frightening)</li>
+          <li>'economists'</li>
+          <li>programming language small-talk </li>
+        </ul>
+      </div>
+    </div>
+</main>
+
